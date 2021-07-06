1d93f829b9fe7ac2f62ae9ebde6047f54aa8821e
Lemon-Ball <86389566+Lemon-Ball@users.noreply.github.com>
2021-07-06 09:17:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+menu_list_array = ["Veg Margherita Pizza", 
+"Cheese Pizza", 
+"Pepperoni Pizza", 
+"Veggie Pizza"
+                    ];
+
+function getmenu(){
+var menu=menu_list_array;
+console.sort(menu);
+console.log(menu);
+
+
+document.getElementById("display_menu").innerHTML=menu;
+
+}
+
+function add_item(){
+var item=document.getElementById("add_item").value;
+console.log(item);
+
+
+
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
